b922e310ba85cd036b3e198527931544d50ffe6c
mariprincipe <79206860+mariprincipe@users.noreply.github.com>
2021-03-30 16:39:05 -0300

    Update AC1.ino
    nova atualização

--- AC1.ino
+++ AC1.ino
@@ -18,9 +18,9 @@ void setup()
   pinMode(vermelho, OUTPUT);
   pinMode(verde, OUTPUT);
   pinMode(azul, OUTPUT);
-  
+
   Serial.begin(9600);
-	
+
   Serial.println("AC1 - Meu Primeiro Projeto 2021");
   Serial.println("                           V1.0");
   Serial.println("Grupo: NOME AQUI               ");
@@ -31,52 +31,55 @@ void loop()
 {
   //toque do botão 1
   if((millis() - lastDebounceTime1) > botaoDelay && digitalRead(botao1)){
-  	Serial.println("produção iniciada");
+      Serial.println("produção iniciada");
     ledVermelho(true);
-  	lastDebounceTime1 = millis();
+      lastDebounceTime1 = millis();
   }
  
   //toque do botão 2
   if((millis() - lastDebounceTime2) > botaoDelay && digitalRead(botao2)){
-  	Serial.println("produção parada");
+      Serial.println("produção parada");
     ledVermelho(false);
-  	lastDebounceTime2 = millis();
+      lastDebounceTime2 = millis();
   }
    //verificador de tempetura
   if(getTemperatura() > 15){
     ledAzul(true);
     Serial.println("Temperatura acima");
   }else{
-  	ledAzul(false); 
+      ledAzul(false); 
     Serial.println("temperatura ok");
 } 
-    	
+
   delay(10);
 }
-
+if(getLuminosidade() > 5){
+    ledVerde(true);
+    Serial.println("Luminosidade acima do correto");
+  }else{
+    ledVerde(false);
+    Serial.println("Luminosidade correta");
+  }
 
 void ledVermelho(bool estado){
   digitalWrite(vermelho,estado);
 }
-void ledVerde(){
- //precisa fazer  
+void ledVerde(bool estado){
+ digitalWrite (verde,estado);
 }
 void ledAzul(bool estado){
-	digitalWrite(azul,estado);
+    digitalWrite(azul,estado);
 }
-
-
 //função de leitura de temperatura
 int getTemperatura(){
-  	int temperaturaC;
-	temperaturaC = map(((analogRead(A0) - 20) * 3.04), 0, 1023, -40, 125);
-  	return temperaturaC;
+      int temperaturaC;
+    temperaturaC = map(((analogRead(A0) - 20) * 3.04), 0, 1023, -40, 125);
+      return temperaturaC;
 } 
 //funcao de leitura da luminosidade
 int getLuminosidade(){
-  	int luminosidade;
-	luminosidade = map(analogRead(A1), 6, 619, -3, 10);
-  	return luminosidade;
+      int luminosidade;
+    luminosidade = map(analogRead(A1), 6, 619, -3, 10);
+      return luminosidade;
 }
-  
   
